04f2f32715ee4f09cc519b2e5a196a19a760544c
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2024-10-11 08:44:14 -0300

    adjusted the models

--- registrations/admin.py
+++ registrations/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+
+
+@admin.register(models.Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+
+
+@admin.register(models.Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+
+
+@admin.register(models.Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["id", "user__username"]
--- registrations/models.py
+++ registrations/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
-from choices import UNIT_OF_MEASUREMENT
+from . import variables_choices
 
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.PROTECT, related_name="Perfil")
-    phone_number = models.CharField(max_length=30, null=True, blank=True)
-    registration_number = models.CharField(max_length=30, verbose_name="CPF ou CNPJ", default="SEM CADASTRO")
-
+    user = models.OneToOneField(User, on_delete=models.PROTECT, related_name="perfil", verbose_name="Usuário")
+    place = models.ForeignKey("Place", on_delete=models.PROTECT,null=True, blank=True, related_name="perfis", verbose_name="Local de lotação")
+    phone_number = models.CharField(max_length=30, null=True, blank=True, verbose_name="Número de telefone")
+    registration_number = models.CharField(
+        max_length=30, verbose_name="CPF ou CNPJ", default="SEM CADASTRO"
+    )
 
     def __str__(self) -> str:
         return self.user.username
@@ -17,27 +19,36 @@ class Profile(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=200, verbose_name="Nome")
     unit_of_measurement = models.CharField(
-        max_length=10, choices=UNIT_OF_MEASUREMENT, verbose_name="Unidade de medida"
+        max_length=10,
+        choices=variables_choices.UNIT_OF_MEASUREMENT,
+        verbose_name="Unidade de medida",
     )
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Data de criação")
     update_at = models.DateTimeField(auto_now=True, verbose_name="Data de atualização")
 
+    def __str__(self):
+        return self.name
 
-class Suppliers(models.Model):
+
+class Supplier(models.Model):
     name = models.CharField(max_length=200, verbose_name="Nome")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Data de criação")
     update_at = models.DateTimeField(auto_now=True, verbose_name="Data de atualização")
-    registration_number = models.CharField(max_length=30, verbose_name="CPF ou CNPJ", default="SEM CADASTRO")
+    registration_number = models.CharField(
+        max_length=30, verbose_name="CPF ou CNPJ", default="SEM CADASTRO"
+    )
 
     def __str__(self):
         return self.name
 
 
 class Place(models.Model):
-    mame = models.CharField(max_length=200, verbose_name="Nome")
+    name = models.CharField(max_length=200, verbose_name="Nome")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Data de criação")
     update_at = models.DateTimeField(auto_now=True, verbose_name="Data de atualização")
-    registration_number = models.CharField(max_length=30, verbose_name="CPF ou CNPJ", default="SEM CADASTRO")
+    registration_number = models.CharField(
+        max_length=30, verbose_name="CPF ou CNPJ", default="SEM CADASTRO"
+    )
 
     def __str__(self):
         return self.name
