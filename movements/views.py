e1769945a3d056e74710774ab1cfe6fa047238ef
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2024-10-15 08:50:33 -0300

    Create views generics

--- movements/views.py
+++ movements/views.py
@@ -1,3 +1,22 @@
-from rest_framework.generics import ListCreateAPIView
-
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import ProductEntry, StockEntry
 # Create your views here.
+
+class ProductEntryLisctCreateApiView(ListCreateAPIView):
+    queryset = ProductEntry.objects.all()
+    serializer_class = None
+
+
+class ProductEntryRetrieveUpdateDestroyApiView(RetrieveUpdateDestroyAPIView):
+    queryset = ProductEntry.objects.all()
+    serializer_class = None
+
+
+class StockEntryLisctCreateApiView(ListCreateAPIView):
+    queryset = StockEntry.objects.all()
+    serializer_class = None
+
+
+class StockEntryRetrieveUpdateDestroyApiView(RetrieveUpdateDestroyAPIView):
+    queryset = StockEntry.objects.all()
+    serializer_class = None
