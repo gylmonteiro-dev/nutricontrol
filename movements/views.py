13f824e341c357fdecbf4333a8b3966188086c20
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2024-10-15 08:55:18 -0300

    Add models serializers in  serializers_class

--- movements/views.py
+++ movements/views.py
@@ -1,22 +1,23 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import ProductEntry, StockEntry
+from .serializers import ProductyEntryModelSerializer, StockEntryModelSerializer
 # Create your views here.
 
 class ProductEntryLisctCreateApiView(ListCreateAPIView):
     queryset = ProductEntry.objects.all()
-    serializer_class = None
+    serializer_class = ProductyEntryModelSerializer
 
 
 class ProductEntryRetrieveUpdateDestroyApiView(RetrieveUpdateDestroyAPIView):
     queryset = ProductEntry.objects.all()
-    serializer_class = None
+    serializer_class = ProductyEntryModelSerializer
 
 
 class StockEntryLisctCreateApiView(ListCreateAPIView):
     queryset = StockEntry.objects.all()
-    serializer_class = None
+    serializer_class = StockEntryModelSerializer
 
 
 class StockEntryRetrieveUpdateDestroyApiView(RetrieveUpdateDestroyAPIView):
     queryset = StockEntry.objects.all()
-    serializer_class = None
+    serializer_class = StockEntryModelSerializer
