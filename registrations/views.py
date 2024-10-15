e3b527355d622666f5014500ce97a3c3c8de484a
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2024-10-15 07:49:30 -0300

    create generic views

--- registrations/views.py
+++ registrations/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
-
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
+from .models import Place, Product, Supplier
 # Create your views here.
+
+
+class ProductListCreateApiView(ListCreateAPIView):
+    queryset = Product.objects.all
+    serializer_class =  None
+
+
+class ProductRetrieveUpdateDestroyApiView(RetrieveUpdateAPIView):
+    queryset = Product.objects.all
+    serializer_class = None
+
+
+class PlaceListCreateApiView(ListCreateAPIView):
+    queryset = Place.objects.all
+    serializer_class = None
+
+
+class PlaceRetrieveUpdateDestroyApiView(RetrieveUpdateAPIView):
+    queryset = Place.objects.all
+    serializer_class = None
+
+
+class SupplierListCreateApiView(ListCreateAPIView):
+    queryset = Supplier.objects.all
+    serializer_class = None
+
+
+class SupplierRetrieveUpdateDestroyApiView(RetrieveUpdateAPIView):
+    queryset = Supplier.objects.all
+    serializer_class = None
